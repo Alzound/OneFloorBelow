2bd656691e2325d707c95a79e0dd36c9e403284c
Alzound <aaxlcd@gmail.com>
2022-04-27 23:11:18 -0500

    Assets y particula

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,9 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    Canvas can;  
+
+    private void Start() 
+    {
+        can = this.gameObject.GetComponent<Canvas>();     
+    }
+
     public void Play()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        can.enabled = false; 
     }
 
     public void Options()
